ab1a716efa79359b504b672d66a11a62fea28935
Manuela-Puerta-A <Manuela.puertaa@uqvirtual.edu.co>
2025-04-02 10:39:53 -0500

    comits

--- src/main/java/co/edu/uniquindio/presupuestoejemplo/proyectofacade/App.java
+++ src/main/java/co/edu/uniquindio/presupuestoejemplo/proyectofacade/App.java
@@ -1,13 +1,29 @@
 package co.edu.uniquindio.presupuestoejemplo.proyectofacade;
-import  co.edu.uniquindio.presupuestoejemplo.proyectofacade.VideoFacade;
 
-public class App {
-	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+	@Override
+	public void start(Stage stage) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("vista.fxml"));
+		Scene scene = new Scene(fxmlLoader.load(), 420, 440);
+		stage.setTitle("Reproductor");
+		stage.setScene(scene);
+		stage.show();
+	}
+
 	public static void main(String[] args) {
 		VideoFacade videoFacade = new VideoFacade();
 
+		launch();
 
 	}
+
 
 
 }
